4c8d84d77b7f10a85619a9fe4732859bc239fa74
Allyson <allysoncesario2018@gmail.com>
2025-03-19 11:45:23 -0300

    Update pacienteRoutes.js

--- pacienteRoutes.js
+++ pacienteRoutes.js
@@ -1,27 +1,9 @@
 const express = require('express');
+const pacienteController =  require('../controller/pacienteController');
 
 const router = express.Router();
 
-const pacienteRoutes = require('./src/routes/pacienteRoutes');
-
-mongoose.connect("mongodb+srv://Kraugel11:<db_Kraugel11>@cluster0.czu1w.mongodb.net/", {
-    serverSelectionTimeoutMS: 300000,
-}
-)
-.then(() => console.log("conectado ao MongoDB com sucesso!"))
-.cath((err) => console.error("erro ao conectar ao mongoDB", err));
-
-
-app.get('/health', (req, res) => {
-    res.status(200).json({ status: 200, mensage: 'servidor ativo!'});
-});
-
-app.use('/api', pacienteRoutes);
-
-const PORT = 300000;
-app.listen(PORT, () =>{
-    console.log(`Servidor rodando na porta ${PORT}`);
-});
-
+router.post('/pacientes', pacienteController.criarPacientes);
+router.get('/pacientes', pacienteController.listarPacientes);
 
 module.exports = router;
